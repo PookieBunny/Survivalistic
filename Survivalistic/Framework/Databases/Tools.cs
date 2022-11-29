711be2de0e1d31023ae02ee9f722e62ab280b119
PookieBunny <aileenmcdaid2001@gmail.com>
2022-11-28 19:35:59 -0800

    Update Tools.cs

--- Survivalistic/Framework/Databases/Tools.cs
+++ Survivalistic/Framework/Databases/Tools.cs
@@ -8,14 +8,14 @@ namespace Survivalistic.Framework.Databases
         {
             Dictionary<string, string> holder = new Dictionary<string, string>();
 
-            holder.Add("Axe", "0.25/0.5");
-            holder.Add("Pickaxe", "0.25/0.5");
-            holder.Add("Hoe", "0.25/0.5");
-            holder.Add("Scythe", "0.1/0.2");
-            holder.Add("Fishing Rod", "0.15/0.3");
-            holder.Add("Watering Can", "0.1/0.2");
-            holder.Add("Shears", "0.15/0.3");
-            holder.Add("Milk Pail", "0.15/0.3");
+            holder.Add("Axe", "0.0/0.0");
+            holder.Add("Pickaxe", "0.0/0.0");
+            holder.Add("Hoe", "0.0/0.0");
+            holder.Add("Scythe", "0.0/0.0");
+            holder.Add("Fishing Rod", "0.0/0.0");
+            holder.Add("Watering Can", "0.0/0.0");
+            holder.Add("Shears", "0.0/0.0");
+            holder.Add("Milk Pail", "0.0/0.0");
 
             return holder;
         }
